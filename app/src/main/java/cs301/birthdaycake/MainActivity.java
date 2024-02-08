a575e137822c29e0b84cdc802c16a826a226bf87
jnguyen1515 <jnguyen1515@gmail.com>
2024-02-08 10:10:10 -0800

    Changed the text of "welcome to cs 371" to Happy birthday. Then changed the color and size

--- app/src/main/java/cs301/birthdaycake/MainActivity.java
+++ app/src/main/java/cs301/birthdaycake/MainActivity.java
@@ -2,15 +2,21 @@ package cs301.birthdaycake;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
+    }
+    public void Goodbye(View button) {
+        Log.i("button", "Goodbye");
     }
 }
